35ae89916aa1df48530319a2ec49bf365d13b2fd
Thomas Alexander <thomasalexander2718@gmail.com>
2026-01-05 09:17:43 -0400

    Remove user install.
    Signed-off-by: Thomas Alexander <thomasalexander2718@gmail.com>

--- scripts/validate_pycudaq.sh
+++ scripts/validate_pycudaq.sh
@@ -230,6 +230,7 @@ fi
 
 # Run backend tests
 echo "Running backend tests."
+python3 -m pip install fastapi uvicorn llvmlite
 for backendTest in "$root_folder/tests/backends"/*.py; do 
     python3 -m pytest -v $backendTest
     # Exit code 5 indicates that no tests were collected,
@@ -273,6 +274,8 @@ else
 fi
 
 # Run snippets in docs
+# Some snippets generate plots
+python3 -m pip install matplotlib
 for ex in `find "$root_folder/snippets" -name '*.py'`; do
     echo "Executing $ex"
     python3 "$ex"
@@ -283,6 +286,8 @@ for ex in `find "$root_folder/snippets" -name '*.py'`; do
 done
 
 # Run examples
+# Some examples generate plots
+python3 -m pip install matplotlib
 for ex in `find "$root_folder/examples" -name '*.py'`; do
     skip_example=false
     explicit_targets=`cat $ex | grep -Po '^\s*cudaq.set_target\("\K.*(?=")'`
